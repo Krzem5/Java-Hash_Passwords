a39510a95cb329bac51ae002ed8f4c35c71ba441
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:12:44 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/hash_passwords/Main.java&&java com/krzem/hash_passwords/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/hash_passwords/Main.java&&jar cvmf ../manifest.mf ../build/hash_passwords.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="hash_passwords.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/hash_passwords.jar
+:end
